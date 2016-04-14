aa204f16b6d65061b2e579f5a95490a09b45134a
Joleen Schwellenbach <schwellenbachjc@my.witc.edu>
2016-04-13 23:40:14 -0500

    Added files via upload

---
+++ A05/src/controller/Controller.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+import view.View;
+
+/**
+ *
+ * @author Joleen Schwellenbach
+ */
+public class Controller {
+        private View view = null;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+    
+}
